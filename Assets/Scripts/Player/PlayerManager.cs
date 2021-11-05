6dd905ebbbd423ae02ef65c283aed88bcbfa5d6f
c0cac01a <joobs1127@gmail.com>
2021-11-06 01:34:44 +0900

    home player edit and players spawn

--- Assets/Scripts/Player/PlayerManager.cs
+++ Assets/Scripts/Player/PlayerManager.cs
@@ -4,9 +4,39 @@ using UnityEngine;
 
 public class PlayerManager : MonoBehaviour
 {
+    public List<Transform> spawnPoints;
+    public List<int> points;
+    private int[,] difficultList = new int[6, 3] 
+    { 
+        { 6, 0, 0 }, { 4, 2, 0 }, { 3, 3, 0 },
+        { 3, 2, 1 }, { 2, 3, 1 }, { 2, 2, 2 } 
+    };
+
+    private void SpawnAwayPlayer(int grade)
+    {
+        switch (grade) 
+        {
+            case 0:
+                break;
+            case 1:
+                break;
+            case 2:
+                break;
+        }
+    }
+
     private void SpawnPlayer()
     {
         // TODO 주사위 데이터 받아서 아군, 적 생성
+        int difficult = 0;
+
+        for (int i = 0; i < 3; ++i)
+        {
+            for (int j = 0; j < difficultList[difficult, i]; ++j)
+            {
+                SpawnAwayPlayer(i);
+            }
+        }
     }
 
     private void Awake()
