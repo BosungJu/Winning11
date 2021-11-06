ddb64fb7244c55fd2c8dc0cf87778e940a067783
c0cac01a <joobs1127@gmail.com>
2021-11-06 12:10:29 +0900

    goal post complete

--- Assets/Scripts/Object/GoalPost.cs
+++ Assets/Scripts/Object/GoalPost.cs
@@ -13,14 +13,21 @@ public class GoalPost : Singleton<GoalPost>
     public GameObject rightPost;
     public GameObject centerPost;
 
-    public void SetPost(int n)
+    public void Awake()
+    {
+        GameManager.instance.startGameEvent += SetPost;
+        SetPost();
+    }
+
+    public void SetPost()
     {
         float width = centerPost.transform.localScale.x;
-        float pos = n / 2 + (n % 2 == 0 ? 0.5f : 0);
+        float pos = -(DiceGameData.blueDice / 2 + (DiceGameData.blueDice % 2 == 0 ? -0.5f : 0)) * width;
 
         Instantiate(leftPost, transform).transform.localPosition = new Vector3(pos - width, 0, 0);
         leftEndPoint = pos - width;
-        for (int i = 0; i < n; ++i)
+        
+        for (int i = 0; i < DiceGameData.blueDice; ++i)
         {
             Instantiate(centerPost, transform).transform.localPosition = new Vector3(pos, 0, 0);
             pos += width;
